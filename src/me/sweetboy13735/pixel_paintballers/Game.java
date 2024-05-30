721ad082fde7da70f204686edc2d1ae184ea303f
Ramone Graham <ramonegraham@gmail.com>
2024-05-30 06:23:25 +0100

    Initialise Java project

---
+++ src/me/sweetboy13735/pixel_paintballers/Game.java
@@ -0,0 +1,15 @@
+package me.sweetboy13735.pixel_paintballers;
+
+/**
+ * Represents the core of the game.
+ * When the program is executed, this class is responsible for handling the game's main functions (E.g. {@code tick} and {@code render}).
+ * @author Ramone Graham
+ */
+public class Game {
+
+	/**
+	 * Starts execution of the program (Called by the JVM).
+	 * @param args Additional arguments to start the game.
+	 */
+	public static void main(String[] args) { System.out.println("Hello, World!"); }
+}
