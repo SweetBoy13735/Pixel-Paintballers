7e6a25f063b0c3e33a187d1017ab635b3484f2e5
Ramone Graham <ramonegraham@gmail.com>
2024-05-30 17:51:50 +0100

    Implement main loop
    - Implemented the main game loop.
    - Created "Window.java".

--- src/me/sweetboy13735/pixel_paintballers/Game.java
+++ src/me/sweetboy13735/pixel_paintballers/Game.java
@@ -1,15 +1,120 @@
 package me.sweetboy13735.pixel_paintballers;
 
+import static java.awt.Color.BLACK;
+
+import java.awt.Canvas;
+import java.awt.Dimension;
+import java.awt.Graphics2D;
+import java.awt.image.BufferStrategy;
+
+import me.sweetboy13735.pixel_paintballers.ui.Window;
+
 /**
  * Represents the core of the game.
  * When the program is executed, this class is responsible for handling the game's main functions (E.g. {@code tick} and {@code render}).
  * @author Ramone Graham
  */
-public class Game {
+public final class Game extends Canvas implements Runnable {
+
+	private static final long serialVersionUID = 1L;
+
+	public static final String TITLE = "Pixel Paintballers";
+	public static final int WIDTH = 800, HEIGHT = WIDTH / 4 * 3;
+
+	private final Window window;
+
+	private boolean running = false;
+	private transient Thread thread;
+
+	/** Creates a new Game instance. */
+	public Game() {
+		Dimension dimension = new Dimension(WIDTH, HEIGHT);
+
+		setMinimumSize(dimension);
+		setMaximumSize(dimension);
+		setPreferredSize(dimension);
+
+		window = new Window(this);
+	}
 
 	/**
 	 * Starts execution of the program (Called by the JVM).
 	 * @param args Additional arguments to start the game.
 	 */
-	public static void main(String[] args) { System.out.println("Hello, World!"); }
+	public static void main(String[] args) { new Game(); }
+
+	/** Starts execution of the game. */
+	public synchronized void start() {
+		running = true;
+		thread = new Thread(this, TITLE + " (Main)");
+
+		thread.start();
+	}
+
+	/** Stops execution of the game. */
+	public synchronized void stop() {
+		running = false;
+
+		try { thread.join(); } catch (InterruptedException exception) { exception.printStackTrace(); }
+	}
+	
+	/** Updates the logic of the game. */
+	private void tick() {}
+
+	/** Updates the visuals of the game. */
+	private void render() {
+		BufferStrategy strategy = getBufferStrategy();
+		
+		if (strategy == null) {
+			createBufferStrategy(3);
+
+			return;
+		}
+
+		Graphics2D graphics = (Graphics2D) strategy.getDrawGraphics();
+
+		graphics.setColor(BLACK);
+		graphics.fill(getBounds());
+
+		graphics.dispose();
+		strategy.show();
+	}
+
+	// See java.lang.Runnable#run()
+	/** {@inheritDoc} */
+	@Override
+	public void run() {
+		requestFocus();
+
+		double desiredTPS = 64, nanosecsPerTick = 1000000000 / desiredTPS, delta = 0;
+		int tps = 0, fps = 0;
+		long timer = System.currentTimeMillis(), lastTime = System.nanoTime();
+
+		while (running) {
+			long now = System.nanoTime();
+			delta += (now-lastTime) / nanosecsPerTick;
+			lastTime = now;
+
+			while (delta >= 1) {
+				tick();
+
+				delta--;
+				tps++;
+			}
+
+			if (running) render();
+
+			fps++;
+
+			if (System.currentTimeMillis()-timer >= 1000) {
+				System.out.println("TPS: " + tps + ". FPS: " + fps + ".");
+
+				timer += 1000;
+				tps = 0;
+				fps = 0;
+			}
+		}
+
+		stop();
+	}
 }
---
+++ src/me/sweetboy13735/pixel_paintballers/ui/Window.java
@@ -0,0 +1,99 @@
+package me.sweetboy13735.pixel_paintballers.ui;
+
+import static java.awt.BorderLayout.CENTER;
+import static me.sweetboy13735.pixel_paintballers.Game.TITLE;
+
+import java.awt.BorderLayout;
+import java.awt.DisplayMode;
+import java.awt.GraphicsDevice;
+import java.awt.GraphicsEnvironment;
+import java.awt.event.WindowEvent;
+import java.awt.event.WindowListener;
+import java.util.ArrayList;
+import java.util.List;
+
+import javax.swing.JFrame;
+
+import me.sweetboy13735.pixel_paintballers.Game;
+
+/**
+ * Represents the window frame of the game.
+ * When constructed, this class is responsible for handling the game's window and any changes to its state.
+ * @author Ramone Graham
+ */
+public final class Window extends JFrame implements WindowListener {
+
+	private static final long serialVersionUID = 1L;
+
+	private final Game game;
+
+	private transient GraphicsDevice screen;
+	private transient List<DisplayMode> displayModes = new ArrayList<>();
+	private boolean fullscreen = false;
+
+	/**
+	 * Creates a new Window.
+	 * @param game The {@link Game} visuals to add to the window.
+	 */
+	public Window(final Game game) {
+		super(TITLE);
+
+		this.game = game;
+
+		screen = GraphicsEnvironment.getLocalGraphicsEnvironment().getDefaultScreenDevice();
+		
+		for (DisplayMode mode : screen.getDisplayModes()) if (mode.getWidth() == Game.WIDTH && mode.getHeight() == Game.HEIGHT) displayModes.add(mode);
+
+		setDefaultCloseOperation(EXIT_ON_CLOSE);
+		setLayout(new BorderLayout());
+		setResizable(false);
+		add(game, CENTER);
+		addWindowListener(this);
+		pack();
+		setLocationRelativeTo(null);
+		setVisible(true);
+
+		game.start();
+	}
+
+	@Override
+	public void windowOpened(WindowEvent e) {}
+
+	@Override
+	public void windowClosing(WindowEvent e) {}
+
+	@Override
+	public void windowClosed(WindowEvent e) {}
+
+	@Override
+	public void windowIconified(WindowEvent e) {}
+
+	@Override
+	public void windowDeiconified(WindowEvent e) {}
+
+	@Override
+	public void windowActivated(WindowEvent e) {}
+
+	@Override
+	public void windowDeactivated(WindowEvent e) {}
+
+	/** Toggles the window's fullscreen mode. */
+	public void toggleFullscreen() {
+		if (!screen.isFullScreenSupported()) return;
+
+		fullscreen = !fullscreen;
+
+		dispose();
+		setUndecorated(fullscreen);
+		pack();
+
+		screen.setFullScreenWindow(fullscreen ? this : null);
+
+		if (!fullscreen) {
+			setLocationRelativeTo(null);
+			setVisible(true);
+		} else if (screen.isDisplayChangeSupported() && !displayModes.isEmpty()) screen.setDisplayMode(displayModes.getFirst());
+
+		game.requestFocus();
+	}
+}
